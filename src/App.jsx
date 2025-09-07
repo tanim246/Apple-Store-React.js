4945c60470d7f57e5d0eb6cd0a634fa107808c55
Tanim <mdtasnimhossaintanim@gmail.com>
2025-09-08 01:50:12 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import Navber from './Shared/Navber'
+
+const App = () => {
+  return (
+    <div className=''>
+      <Navber />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Shared/Navber.jsx
@@ -0,0 +1,53 @@
+import { FaRegHeart, FaRegUser } from "react-icons/fa";
+import { LuShoppingCart } from "react-icons/lu";
+import { MdOutlineSearch } from "react-icons/md";
+import { TfiHeadphoneAlt } from "react-icons/tfi";
+
+const Navber = () => {
+  return (
+    <div>
+      <div className="flex justify-between items-center  max-w-6xl mx-auto border py-2 mt-2 ">
+        {/* navber-start */}
+        <div className="flex items-center gap-16">
+          <h1 className="text-3xl font-bold text-[#d52345]">E N T R Y.</h1>
+          <div className="relative w-[600px]">
+            <input
+              type="text"
+              className="border-gray-400 border px-4 pr-12 rounded-full w-full py-2 focus:outline-[#d62243] focus:outline-1"
+              placeholder="Search..."
+            />
+
+            <button className="absolute right-1 top-1/2 -translate-y-1/2 bg-[#d62243] rounded-full w-8 h-8 flex items-center justify-center cursor-pointer">
+              <MdOutlineSearch className="text-white w-5 h-5" />
+            </button>
+          </div>
+        </div>
+        {/* naver-end */}
+        <div className="flex  gap-10">
+          <div className="flex gap-2 items-center">
+            <span>
+              <TfiHeadphoneAlt className=" text-black w-8 h-8 p-1 " />
+            </span>
+            <div className="flex flex-col text-black text-[12px]">
+              <span>entry@gmail.com</span>
+              <span>(5447 45 421 251)</span>
+            </div>
+          </div>
+          <div className="flex space-x-3 items-center ">
+            <span className=" w-8 h-8 p-1 text-black flex justify-center items-center ">
+              <FaRegUser className="hover:text-[#d62243] text-xl cursor-pointer" />
+            </span>
+            <span className=" w-8 h-8 p-1 text-black flex justify-center items-center ">
+              <FaRegHeart className="hover:text-[#d62243] text-xl cursor-pointer" />
+            </span>
+            <span className=" w-8 h-8 p-1 text-black flex justify-center items-center ">
+              <LuShoppingCart className="hover:text-[#d62243] text-xl cursor-pointer" />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navber;
